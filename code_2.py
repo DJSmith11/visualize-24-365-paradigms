c71dfee51e3825667cfca4f5fcbabe499c9e0c62
DJSmith11 <142458119+DJSmith11@users.noreply.github.com>
2023-11-18 00:30:14 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,66 @@
+    for item in data:
+    return data
+
+import random
+    main()
+
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+
+def generate_random_data():    data = generate_random_data()
+
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+        print(f"Random Number: {item}")
+    main()
+    return data
+
+if __name__ == "__main__":
+    for item in data:
+def generate_random_data():
+def main():
+
+
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+    return data
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+    for item in data:
+if __name__ == "__main__":
+import random
+def main():
+    main()
+    main()
+    for item in data:
+    return data
+
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+def generate_random_data():
+def main():
+
+
+    data = generate_random_data()
+import random
+
+
+    main()
+        print(f"Random Number: {item}")
+    for item in data:
+
+    return data
+if __name__ == "__main__":
+import random
+def generate_random_data():
+
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
